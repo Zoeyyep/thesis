f0b6c234b6113f6f1dbeb4f33fdead116450bf4a
Zoeyyep <113126104+Zoeyyep@users.noreply.github.com>
2022-09-08 17:26:04 +0100

    step2_data preprocessing

---
+++ classpre.py
@@ -0,0 +1,321 @@
+from textblob import TextBlob
+from nltk.corpus import stopwords
+from string import punctuation
+import pandas as pd
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import wordnet
+pd.options.mode.chained_assignment = None
+
+unused_words= pd.read_csv('/Users/zhaoziyi/Desktop/sma/step1_sample/stop_words.csv',encoding='utf8')
+stop_words= stopwords.words('english')
+for w in unused_words['Words']:
+    stop_words.append(w)
+
+contractions_dict = {
+    "ain't": "am not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "can't've": "cannot have",
+    "'cause": "because",
+    "could've": "could have",
+    "couldn't": "could not",
+    "couldn't've": "could not have",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "don’t": "do not",
+    "hadn't": "had not",
+    "hadn't've": "had not have",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he had",
+    "he'd've": "he would have",
+    "he'll": "he will",
+    "he'll've": "he will have",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'd'y": "how do you",
+    "how'll": "how will",
+    "how's": "how is",
+    "i'd": "i would",
+    "i'd've": "i would have",
+    "i'll": "i will",
+    "i'll've": "i will have",
+    "i'm": "i am",
+    "i've": "i have",
+    "isn't": "is not",
+    "it'd": "it would",
+    "it'd've": "it would have",
+    "it'll": "it will",
+    "it'll've": "it will have",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "mayn't": "may not",
+    "might've": "might have",
+    "mightn't": "might not",
+    "mightn't've": "might not have",
+    "must've": "must have",
+    "mustn't": "must not",
+    "mustn't've": "must not have",
+    "needn't": "need not",
+    "needn't've": "need not have",
+    "o'clock": "of the clock",
+    "oughtn't": "ought not",
+    "oughtn't've": "ought not have",
+    "shan't": "shall not",
+    "sha'n't": "shall not",
+    "shan't've": "shall not have",
+    "she'd": "she would",
+    "she'd've": "she would have",
+    "she'll": "she will",
+    "she'll've": "she will have",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "shouldn't've": "should not have",
+    "so've": "so have",
+    "so's": "so is",
+    "that'd": "that would",
+    "that'd've": "that would have",
+    "that's": "that is",
+    "there'd": "there would",
+    "there'd've": "there would have",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'd've": "they would have",
+    "they'll": "they will",
+    "they'll've": "they will have",
+    "they're": "they are",
+    "they've": "they have",
+    "to've": "to have",
+    "wasn't": "was not",
+    "we'd": "we would",
+    "we'd've": "we would have",
+    "we'll": "we will",
+    "we'll've": "we will have",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'll": "what will",
+    "what'll've": "what will have",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "when's": "when is",
+    "when've": "when have",
+    "where'd": "where did",
+    "where's": "where is",
+    "where've": "where have",
+    "who'll": "who will",
+    "who'll've": "who will have",
+    "who's": "who is",
+    "who've": "who have",
+    "why's": "why is",
+    "why've": "why have",
+    "will've": "will have",
+    "won't": "will not",
+    "won't've": "will not have",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "wouldn't've": "would not have",
+    "y'all": "you all",
+    "y’all": "you all",
+    "y'all'd": "you all would",
+    "y'all'd've": "you all would have",
+    "y'all're": "you all are",
+    "y'all've": "you all have",
+    "you'd": "you would",
+    "you'd've": "you would have",
+    "you'll": "you will",
+    "you'll've": "you will have",
+    "you're": "you are",
+    "you've": "you have",
+    "ain’t": "am not",
+    "aren’t": "are not",
+    "can’t": "cannot",
+    "can’t’ve": "cannot have",
+    "’cause": "because",
+    "could’ve": "could have",
+    "couldn’t": "could not",
+    "couldn’t’ve": "could not have",
+    "didn’t": "did not",
+    "doesn’t": "does not",
+    "don’t": "do not",
+    "don’t": "do not",
+    "hadn’t": "had not",
+    "hadn’t’ve": "had not have",
+    "hasn’t": "has not",
+    "haven’t": "have not",
+    "he’d": "he had",
+    "he’d’ve": "he would have",
+    "he’ll": "he will",
+    "he’ll’ve": "he will have",
+    "he’s": "he is",
+    "how’d": "how did",
+    "how’d’y": "how do you",
+    "how’ll": "how will",
+    "how’s": "how is",
+    "i’d": "i would",
+    "i’d’ve": "i would have",
+    "i’ll": "i will",
+    "i’ll’ve": "i will have",
+    "i’m": "i am",
+    "i’ve": "i have",
+    "isn’t": "is not",
+    "it’d": "it would",
+    "it’d’ve": "it would have",
+    "it’ll": "it will",
+    "it’ll’ve": "it will have",
+    "it’s": "it is",
+    "let’s": "let us",
+    "ma’am": "madam",
+    "mayn’t": "may not",
+    "might’ve": "might have",
+    "mightn’t": "might not",
+    "mightn’t’ve": "might not have",
+    "must’ve": "must have",
+    "mustn’t": "must not",
+    "mustn’t’ve": "must not have",
+    "needn’t": "need not",
+    "needn’t’ve": "need not have",
+    "o’clock": "of the clock",
+    "oughtn’t": "ought not",
+    "oughtn’t’ve": "ought not have",
+    "shan’t": "shall not",
+    "sha’n’t": "shall not",
+    "shan’t’ve": "shall not have",
+    "she’d": "she would",
+    "she’d’ve": "she would have",
+    "she’ll": "she will",
+    "she’ll’ve": "she will have",
+    "she’s": "she is",
+    "should’ve": "should have",
+    "shouldn’t": "should not",
+    "shouldn’t’ve": "should not have",
+    "so’ve": "so have",
+    "so’s": "so is",
+    "that’d": "that would",
+    "that’d’ve": "that would have",
+    "that’s": "that is",
+    "there’d": "there would",
+    "there’d’ve": "there would have",
+    "there’s": "there is",
+    "they’d": "they would",
+    "they’d’ve": "they would have",
+    "they’ll": "they will",
+    "they’ll’ve": "they will have",
+    "they’re": "they are",
+    "they’ve": "they have",
+    "to’ve": "to have",
+    "wasn’t": "was not",
+    "we’d": "we would",
+    "we’d’ve": "we would have",
+    "we’ll": "we will",
+    "we’ll’ve": "we will have",
+    "we’re": "we are",
+    "we’ve": "we have",
+    "weren’t": "were not",
+    "what’ll": "what will",
+    "what’ll’ve": "what will have",
+    "what’re": "what are",
+    "what’s": "what is",
+    "what’ve": "what have",
+    "when’s": "when is",
+    "when’ve": "when have",
+    "where’d": "where did",
+    "where’s": "where is",
+    "where’ve": "where have",
+    "who’ll": "who will",
+    "who’ll’ve": "who will have",
+    "who’s": "who is",
+    "who’ve": "who have",
+    "why’s": "why is",
+    "why’ve": "why have",
+    "will’ve": "will have",
+    "won’t": "will not",
+    "won’t’ve": "will not have",
+    "would’ve": "would have",
+    "wouldn’t": "would not",
+    "wouldn’t’ve": "would not have",
+    "y’all": "you all",
+    "y’all": "you all",
+    "y’all’d": "you all would",
+    "y’all’d’ve": "you all would have",
+    "y’all’re": "you all are",
+    "y’all’ve": "you all have",
+    "you’d": "you would",
+    "you’d’ve": "you would have",
+    "you’ll": "you will",
+    "you’ll’ve": "you will have",
+    "you’re": "you are",
+    "you’re": "you are",
+    "you’ve": "you have",
+}
+contractions_re = re.compile('(%s)' % '|'.join(contractions_dict.keys()))
+
+class Preprocessor:
+    @staticmethod
+    def cleanhtml(raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        rem_num = re.sub('[0-9]+', '', cleantext)
+        return rem_num
+
+    @staticmethod
+    # Function expand the contractions if there's any
+    def expand_contractions(s, contractions_dict=contractions_dict):
+        def replace(match):
+            return contractions_dict[match.group(0)]
+
+        return contractions_re.sub(replace, s)
+
+    @staticmethod
+    #  Get the lexical properties of words
+    def get_wordnet_pos(tag):
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return None
+
+    @staticmethod
+    # Function process the data
+    def text_processing(df):
+        n = len(df['Article'])
+        df['Article'] = df['Article'].apply(lambda x: Preprocessor.cleanhtml(str(x)))
+        df['Article'] = df['Article'].apply(lambda x: Preprocessor.expand_contractions(str(x)))
+        df['Article'] = df['Article'].apply(lambda x: word_tokenize(str(x)))
+        df['Article'] = df['Article'].apply(lambda x: ' '.join([word for word in x if word not in (stop_words)]))
+
+        for i in range(n):
+            lemmatizer = WordNetLemmatizer()
+            Text = df['Article'][i].split()
+            tagged_sent = pos_tag(Text)  # Get the lexical properties of words
+            words = []
+            for tag in tagged_sent:
+                wordnet_pos = Preprocessor.get_wordnet_pos(tag[1]) or wordnet.NOUN
+                words.append(lemmatizer.lemmatize(tag[0], pos=wordnet_pos))
+            df['Article'][i] = words
+            # print(df['Article'][i])
+
+        # Removing the punctuation
+        df['Article'] = df['Article'].apply(lambda x: ''.join(word for word in str(x) if word not in punctuation))
+
+        # Removing the whitespace and double spaces
+        df['Article'] = df['Article'].apply(lambda x: re.sub(' +', ' ', x))
+
+        df['Article'] = df['Article'].astype(str)
+        df['Article'] = df['Article'].apply(lambda x: str(TextBlob(x).correct()))
+
+        index = df[df['Article'] == 'nan'].index
+        df.drop(index, inplace=True)
+        df.to_csv('sample0905.csv', encoding="utf-8")
+        return df
